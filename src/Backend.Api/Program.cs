ab73472632a33d18b6468ced7dc367dd1b1c71df
Roberto Sousa <roberto_sdn@yahoo.com.br>
2026-09-20 12:30:19 -0300

    feat: init .net

--- src/Backend.Api/Program.cs
+++ src/Backend.Api/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddProblemDetails();
 builder.Services.AddHealthChecks();
+builder.Services.AddHttpsRedirection(options => options.HttpsPort = 8443);
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
